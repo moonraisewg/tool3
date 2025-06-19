fd6e81095edc3b302ed74adedc208fe0f26b9b6f
conghung294 <conghung294@gmail.com>
2025-06-19 12:36:49 +0700

    feat: update config sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,7 @@ module.exports = {
     siteUrl: "https://tool3.xyz",
     generateRobotsTxt: true,
     changefreq: "weekly",
+    generateIndexSitemap: false,
     robotsTxtOptions: {
         policies: [
             {
